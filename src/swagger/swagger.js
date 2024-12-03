e0f18334ad768e893c909f62c2481a94d7363e0c
wilsonBYU <wilsonomar@byui.edu>
2024-12-03 01:30:28 -0600

    swager ui implemented

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -3,7 +3,8 @@ const swaggerAutogen = require("swagger-autogen")()
 const doc = {
   info: {
     title: "LibMaS API documentation", 
-    description: "Final group project for BYU-I CSE341"
+    description: "Final group project for BYU-I CSE341",
+    version: "0.1a"
   }
 }
 const outputFile = "./documentation.json"
