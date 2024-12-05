84b358d51603fb09217b8c9108b44fa255a38286
wilsonBYU <wilsonomar@byui.edu>
2024-12-04 20:56:23 -0600

    Removed homescreen req log

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   // #swagger.tags = ['Home']
-  console.log(req);
   res.status(200).send("This is the home screen!");
 });
 router.use("/books", require("./books"));
