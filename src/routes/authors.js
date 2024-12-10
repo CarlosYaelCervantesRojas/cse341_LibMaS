735848e2f00d1dfd9546dae15f948c6e896864e0
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2024-12-09 22:19:04 -0800

    added author validation

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -1,11 +1,26 @@
 const router = require("express").Router();
 const authorsController = require("../controllers/authors");
+const auth = require("../utils/auth");
+const authorValidator = require("../utils/authorValidator");
+const checkResult = require("../utils/validate");
 
 router
   .get("/", authorsController.getAll)
   .get("/:author_id", authorsController.get)
-  .post("/", authorsController.post)
-  .put("/:author_id", authorsController.put)
-  .delete("/:author_id", authorsController.delete);
+  .post(
+    "/",
+    auth.isAuthenticated,
+    authorValidator.postRules(),
+    checkResult,
+    authorsController.post
+  )
+  .put(
+    "/:author_id",
+    auth.isAuthenticated,
+    authorValidator.postRules(),
+    checkResult,
+    authorsController.put
+  )
+  .delete("/:author_id", auth.isAuthenticated, authorsController.delete);
 
 module.exports = router;
