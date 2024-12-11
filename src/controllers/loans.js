3dece7986f843d1f83ae0d013ca896bd78df0c8f
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2024-12-11 15:07:37 -0800

    Updated loan controller, error handling, and validation

--- src/controllers/loans.js
+++ src/controllers/loans.js
@@ -1,102 +1,214 @@
 const { ObjectId } = require("mongodb");
 const Database = require("../models/dbClient");
 const db = new Database();
+const { statusCodes } = require("../utils/index");
 
 const COLLECTION = "loans";
 const loansController = {};
 
 // Retrieves all loans - Uses "get" function from dbClient and populates the collection name, leaves qry param empty to retrieve all records
 loansController.getAll = async (req, res) => {
-  // #swagger.tags = ['Loans']
+  /* 
+    #swagger.tags = ['Loans']
+    #swagger.description = 'Returns all loans from the database'
+    #swagger.responses[200] = { description: 'Loans retrieved successfully' }
+    #swagger.responses[500] = { description: 'Internal server error (database or Node.js)' }
+  */
   try {
     const loans = await db.get(COLLECTION);
     res.status(200).json(loans);
   } catch (e) {
     console.error(e.message);
+    res
+      .status(statusCodes.InternalServerError)
+      .json({ error: "internal server error" });
   }
 };
 
 // Retrieves loan - Uses "get" function from dbClient and populates the collection name, populating qry param with specific ID
 loansController.get = async (req, res) => {
-  // #swagger.tags = ['Loans']
+  /* 
+    #swagger.tags = ['Loans']
+    #swagger.description = "Returns loans with the given ID"
+    #swagger.responses[200] = { description: "Loans with the given id retrieval successfully" }
+    #swagger.responses[500] = { description: "Internal server error (database or Node.js)" }
+  */
   try {
-    const loan = await db.get(COLLECTION, {
-      _id: new ObjectId(req.params.loan_id),
-    });
-    if (!loan) {
-      return res.status(404).json({ message: "loan not found" });
+    const loanId = req.params.loan_id;
+
+    if (!ObjectId.isValid(loanId)) {
+      return res
+        .status(statusCodes.BadRequest)
+        .json({ message: "Invalid loan ID" });
     }
 
-    res.status(200).json(loan);
+    const loan = await db.get(COLLECTION, {
+      _id: new ObjectId(loanId),
+    });
+
+    if (!loan) {
+      return res
+        .status(statusCodes.BadRequest)
+        .json({ message: "loan not found" });
+    }
+
+    res.status(statusCodes.OK).json(loan);
   } catch (e) {
     console.error(e.message);
+    res
+      .status(statusCodes.InternalServerError)
+      .json({ error: "internal server error" });
   }
 };
 
 // Adds loan - Defines loan fields and uses "post" function from dbClient to add new loan
 loansController.post = async (req, res) => {
-  // #swagger.tags = ['Loans']
+  /* 
+  #swagger.tags = ['Loans']
+  #swagger.description = "Adds a new loan to the database"
+    #swagger.parameters["body"] = {
+      in: "body",
+      description: "JSON containing loan data",
+      schema: {
+        "bookItemId": "123456",
+        "userId": "654321",
+        "checkOutDate": "2024-12-01",
+        "dueDate": "2024-12-22"
+      }
+    }
+    #swagger.responses[201] = { description: "New loan created successfully" }
+    #swagger.responses[500] = { description: "Internal server error (database or Node.js)" }
+  */
+
   try {
-    const { bookId, userId, checkOutDate, dueDate, returnDate, feeAssessed } =
-      req.body;
+    const { bookId, userId, checkOutDate, dueDate } = req.body;
+
+    if (!bookId || !userId) {
+      return res
+        .status(statusCodes.BadRequest)
+        .json({ message: "Missing required fields: bookId or userId" });
+    }
 
     // calculates the default dueDate if one isn't provided (based on 21 days from checkOutDate)
     const calcDueDate = (checkedOut) => {
       const date = new Date(checkedOut);
       date.setDate(date.getDate() + 21);
-      return date;
+      return date.toISOString().split("T")[0];
     };
 
     // Use today's date if checkOutDate is not provided (helps calculate due date)
-    const effectiveDate = checkOutDate || new Date();
+    const effectiveCheckOutDate = checkOutDate
+      ? new Date(checkOutDate).toISOString().split("T")[0]
+      : new Date().toISOString().split("T")[0];
+
+    // Calculates Due Date if not expliciately provided
+    const effectiveDueDate = dueDate || calcDueDate(effectiveCheckOutDate);
 
     const loan = {
       bookId,
       userId,
-      checkOutDate: effectiveDate,
-      dueDate: dueDate || calcDueDate(effectiveDate), // calculates based on checkout date (or today's date if not provided)
-      returnDate: returnDate || null,
-      feeAssessed: feeAssessed || null,
+      checkOutDate: effectiveCheckOutDate,
+      dueDate: effectiveDueDate,
     };
 
     const result = await db.post(COLLECTION, loan);
-    res.status(201).json({ id: result.insertedId, ...loan });
+    res.status(statusCodes.Created).json({ id: result.insertedId, ...loan });
   } catch (e) {
     console.error(e.message);
-    res.status(500).json({ message: "Failed to add loan" });
+    res
+      .status(statusCodes.InternalServerError)
+      .json({ message: "Failed to add loan" });
   }
 };
 
 // Edits loan - Uses "put" function from dbClient and populates the collection name, populating qry param with specific ID
 loansController.put = async (req, res) => {
-  // #swagger.tags = ['Loans']
+  /* 
+    #swagger.tags = ['Loans']
+    #swagger.description = "Update an existing loan to reflect "returned". Calculates a fee if late. Fields not provided will not be changed."
+    #swagger.parameters["body"] = {
+      in: "body",
+      description: "JSON containing loan data",
+      schema: {
+        "returnDate": "2024-12-20",
+        "feeAssessed": "2.50"
+      }
+    }
+    #swagger.response[200] = {description: "Loan updated successfully"}
+    #swagger.response[400] = {description: "Bad request (missing or invalid data)"}
+    #swagger.response[500] = {description: "Internal server error (database or Node.js)"}
+  */
+  const loanID = req.params.loan_id;
+  const updatedData = req.body;
+
   try {
-    const loan = await db.put(
+    // fetch the existing loan
+    const loan = await db.get(COLLECTION, { _id: new ObjectId(loanID) });
+
+    // check that the loan exists
+    if (!loan) {
+      return res
+        .status(statusCodes.NotFound)
+        .json({ message: "Loan not found" });
+    }
+
+    // Create a default returnDate of today if not provided
+    const today = new Date().toISOString().split("T")[0];
+    const returnDate = updatedData.returnDate || today;
+
+    // Calculate a Late Fee if return date is later than the due date ($0.25 per day), unless explicately provided
+    let feeAssessed = loan.feeAssessed || 0;
+    if (loan.dueDate && new Date(returnDate) > new Date(loan.dueDate)) {
+      const daysLate = Math.ceil(
+        (new Date(returnDate) - new Date(loan.dueDate)) / (1000 * 60 * 60 * 24)
+      );
+      feeAssessed = parseFloat((daysLate * 0.25).toFixed(2));
+    }
+
+    // put the updated values in an object variable
+    const updateFields = {
+      ...updatedData,
+      returnDate,
+      feeAssessed,
+    };
+
+    // update the database
+    const result = await db.put(
       COLLECTION,
-      { _id: new ObjectId(req.params.loan_id) },
-      { $set: req.body }
+      { _id: new ObjectId(loanID) },
+      { $set: updateFields }
     );
 
-    if (loan.matchedCount === 0) {
-      return res.status(404).json({ message: "loan not found" });
+    // check for errors and provide status
+    if (result.matchedCount === 0) {
+      return res
+        .status(statusCodes.NotFound)
+        .json({ message: "Loan not found" });
     }
 
-    if (loan.modifiedCount === 0) {
-      return res.status(200).json({ message: "Nothing to modify" });
+    if (result.modifiedCount === 0) {
+      return res
+        .status(statusCodes.OK)
+        .json({ message: "Nothing was modified" });
     }
 
-    res.status(200).json({ message: "loan updated" });
+    res.status(statusCodes.OK).json({ message: "Loan updated" });
   } catch (e) {
     console.error(e.message);
     res
-      .status(500)
+      .status(statusCodes.InternalServerError)
       .json({ message: "An error occurred while updating the loan" });
   }
 };
 
 // Deletes loan - Uses "delete" function from dbClient and populates the collection name, populating qry param with specific ID
 loansController.delete = async (req, res) => {
-  // #swagger.tags = ['Loans']
+  /* 
+    #swagger.tags = ['Loans']
+    #swagger.description = "Deletes an loan with the given ID"
+    #swagger.response[200] = {description: "Loan deleted succesfully"}
+    #swagger.response[500] = {description: "Internal server error (database or Node.js)"}
+  */
   try {
     const loan = await db.delete(COLLECTION, {
       _id: new ObjectId(req.params.loan_id),
@@ -106,11 +218,11 @@ loansController.delete = async (req, res) => {
       return res.status(404).json({ message: "loan not found" });
     }
 
-    res.status(204).send();
+    res.status(statusCodes.NoContent).send();
   } catch (e) {
     console.error(e.message);
     res
-      .status(500)
+      .status(statusCodes.InternalServerError)
       .json({ message: "An error occurred while deleting the loan" });
   }
 };
--- src/routes/loans.js
+++ src/routes/loans.js
@@ -1,11 +1,26 @@
 const router = require("express").Router();
 const loansController = require("../controllers/loans");
+const auth = require("../utils/auth");
+const loanValidator = require("../utils/loanValidator");
+const checkResult = require("../utils/validate");
 
 router
   .get("/", loansController.getAll)
   .get("/:loan_id", loansController.get)
-  .post("/", loansController.post)
-  .put("/:loan_id", loansController.put)
-  .delete("/:loan_id", loansController.delete);
+  .post(
+    "/",
+    auth.isAuthenticated,
+    loanValidator.postRules(),
+    checkResult,
+    loansController.post
+  )
+  .put(
+    "/:loan_id",
+    auth.isAuthenticated,
+    loanValidator.putRules(),
+    checkResult,
+    loansController.post
+  )
+  .delete("/:loan_id", auth.isAuthenticated, loansController.delete);
 
 module.exports = router;
