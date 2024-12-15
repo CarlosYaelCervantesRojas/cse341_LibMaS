a2a8ba1980bb671ba22163a0c4f0bbe4f4e8c541
wilsonBYU <wilsonomar@byui.edu>
2024-12-14 19:21:17 -0600

    Swagger oauth2, readme file

---
+++ README.md
@@ -0,0 +1,27 @@
+# About
+
+LibMaS is an acronym for Library Management System. The project was created for BYU-I CIT341 Final Group Project.
+
+## How to run the project?
+
+In your env file add the following variables:
+
+- PORT
+- MONGODB_URL
+- GOOGLE_CLIENT_ID
+- GOOGLE_SECRET
+- CALLBACK_URL
+
+Install the dependencies
+
+- `npm install`
+
+Run the project:
+
+- `npm start`
+
+# Authors
+
+  <a href="https://github.com/wilsonBYU/LibMaS/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=wilsonBYU/LibMaS" />
+  </a>
--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -3,10 +3,20 @@ const swaggerAutogen = require("swagger-autogen")();
 const doc = {
   info: {
     title: "LibMaS API documentation",
-    description: "Final group project for BYU-I CSE341",
+    description: "Final group project for BYU-I CSE341. Authors: [Deana Briggs](https://github.com/deanabriggs), [Carlos Cervantes](https://github.com/CarlosYaelCervantesRojas), [Wilson Romero](https://github.com/wilsonBYU)",
     version: "0.1a",
   },
   schemes: ["https", "http"],
+  securityDefinitions: {
+    google_oauth: {
+      type: 'oauth2',
+      flow: 'implicit',
+      authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth",
+      scopes: {
+        profile: 'Read your google profile data',
+      }
+    }
+  }
 };
 
 //
