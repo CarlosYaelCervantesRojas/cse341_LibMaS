886aa396f3609dde96bc2a0af44c1b9a4980c7e1
CarlosYaelCervantesRojas <carlos.yael.cervantes.rojas@gmail.com>
2024-12-14 18:59:25 -0600

    GET routes supertest

--- __test__/get.loan.spec.js
+++ __test__/get.loan.spec.js
@@ -5,14 +5,14 @@ const request = supertest(app)
 
 
 describe('Test Handlers', () => {
-    test('responds to /books/', async () => {
-        const res = await request.get('/books/')
+    test('responds to /loans/', async () => {
+        const res = await request.get('/loans/')
         expect(res.header['content-type']).toBe('application/json; charset=utf-8')
         expect(res.statusCode).toBe(200)
     })
 
-    test('responds to /books/:book_id', async () => {
-        const res = await request.get('/books/675765afb721278eaa85c99c');
+    test('responds to /loans/:loan_id', async () => {
+        const res = await request.get('/loans/675e24a0ff8a08822afcdb6d');
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200)
     })
